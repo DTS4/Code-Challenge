0237c259d953ed7a6f1982cb7ccdcc53f8e66b3a
Keith <keith.githinji@student.moringaschool.com>
2024-09-27 15:05:52 +0300

    Completed Work

--- script-01.js
+++ script-01.js
@@ -1,2 +1,15 @@
-
-
+ let grade = parseInt(prompt("Enter your grade (0 - 100):"));
+const marks = (grade >= 0 && grade <= 100)
+if (marks) {
+    if (grade > 79) {
+        console.log("A");
+    } else if (grade >= 60) {
+        console.log("B");
+    } else if (grade >= 49) {
+        console.log("C");
+    } else if (grade >= 40) {
+        console.log("D");
+    } else {
+        console.log("E");
+    }
+}
