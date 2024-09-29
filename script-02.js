028610987035515e28d25b19821c533f26cb17e4
Keith <keith.githinji@student.moringaschool.com>
2024-09-29 19:47:35 +0300

    Finished code-challenge

--- script-02.js
+++ script-02.js
@@ -11,11 +11,11 @@ function speedDetector(speed) {
         const demeritPoints = Math.floor((speed - speedLimit) / speedDemeritPoint);
 
         if (demeritPoints > 12) {
-            console.log("Suspend License")
+            console.log("License Suspended")
         } else {
             console.log("Points: " + demeritPoints);
         }
     }
 }
-console.log(speedDetector(80))
+console.log(speedDetector(100))
 
